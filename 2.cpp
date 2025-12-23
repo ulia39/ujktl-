162613eb6c350dca33dc3177c4cb12e78468f810
труженик завода <ulia35df@gmail.com>
2025-12-23 15:25:50 +0300

    Create 2.cpp

---
+++ 2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main() {
+	setlocale(0, "");
+	cout << "1. Осень" << endl;
+	cout << "2. Зима" << endl;
+	cout << "3. Весна" << endl;
+	cout << "4. Лето" << endl;
+	cout << "Введите число от 1 до 4 включительно" << endl;
+	int menu;
+	cin >> menu;
+	if (menu == 1) {
+		cout << "Вы выбрали Осень";{
+	}
+	if (menu == 2) {
+		cout << "Вы выбрали Зиму";{
+	}
+	if (menu == 3) {
+		cout << "Вы выбрали Весну";{
+	}
+	if (menu == 4) {
+		cout << "Вы выбрали Лето";{
+	}
+}
